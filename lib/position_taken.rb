cdbcdf289968aafdf5a10df3c2b6dc44c45e5ca5
Mimi Nguyen <mytmimi@gmail.com>
2018-09-30 17:50:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
 # code your #position_taken? method here!
+puts "Welcome to Tic Tac Toe!"
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+puts "Where would you like to go?"
+
+user_input = gets.strip
+
+index = input_to_index(user_input)
+
+move(board, index, "X")
+
+display_board(board)
+
+def position_taken?(index)
+  if board[index] == " "
+    puts "The space is empty"
+  else
+    puts "The space is taken"
+  end
+  
